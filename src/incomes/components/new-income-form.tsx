034bf5c30bcd94a2ee9535f4aaa4a3ddb78a96a8
ArielSurco <arielsurcoa@gmail.com>
2025-01-04 23:34:08 -0300

    feat: new income form

--- src/incomes/components/new-income-form.tsx
+++ src/incomes/components/new-income-form.tsx
@@ -1,3 +1,28 @@
+import Form from 'next/form'
+
+import { AccountField } from '@/expenses/components/account-field'
+import { Button } from '@/shared/components/button'
+import { DatePicker } from '@/shared/components/date-picker'
+import { Input } from '@/shared/components/input'
+import { NumberInput } from '@/shared/components/number-input'
+import { TextArea } from '@/shared/components/text-area'
+
+const action = async (formData: FormData) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  console.log(Array.from(formData.entries()))
+}
+
 export function NewIncomeForm() {
-  return <div className='bg-white text-black'>Income</div>
+  return (
+    <Form action={action} className='flex flex-col gap-4'>
+      <Input name='title' placeholder='Title' />
+      <AccountField />
+      <div className='flex gap-4'>
+        <NumberInput allowNegative={false} autoComplete='off' name='amount' placeholder='Amount' />
+        <DatePicker defaultValue={new Date()} name='date' />
+      </div>
+      <TextArea name='description' placeholder='Description' />
+      <Button type='submit'>Add income</Button>
+    </Form>
+  )
 }
